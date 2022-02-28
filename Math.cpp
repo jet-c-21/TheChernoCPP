3adea3fb9b401411b9eec347473b1680806caaad
jet-c-21 <67193495+jet-c-21@users.noreply.github.com>
2022-02-28 16:55:27 +0800

    fin "How the C++ Compiler Works"

---
+++ Math.cpp
@@ -0,0 +1,9 @@
+//
+// Author: Jet Chien
+// GitHub: https://github.com/jet-chien
+// Create Date: 2/28/22
+//
+int Multiply(int a, int b) {
+  int res = a * b;
+  return res;
+}
---
+++ endbrace.hpp
@@ -0,0 +1,6 @@
+//
+// Author: Jet Chien
+// GitHub: https://github.com/jet-chien
+// Create Date: 2/28/22
+//
+}
