cde403a81f3a66f3756c573300b769f26cd5a332
Jet (Ta-Wei Chien)$PUFF-X <67193495+jet-c-21@users.noreply.github.com>
2022-10-30 02:32:35 +0800

    fin ep41

---
+++ ep41_this_kw_in_cpp/this_kw_in_cpp.cpp
@@ -0,0 +1,25 @@
+//
+// Created by puff on 2022-10-30.
+//
+
+#include "this_kw_in_cpp.hpp"
+
+
+class EntityEp41{
+ public:
+  int x, y;
+  EntityEp41(int x, int y){
+    this->x = x;
+    this->y = y;
+
+  }
+
+};
+
+void PrintEntity(EntityEp41* e) {
+
+}
+
+void this_kw_in_cpp_main() {
+
+}
---
+++ ep41_this_kw_in_cpp/this_kw_in_cpp.hpp
@@ -0,0 +1,6 @@
+//
+// Created by puff on 2022-10-30.
+//
+#include <iostream>
+
+void this_kw_in_cpp_main();
