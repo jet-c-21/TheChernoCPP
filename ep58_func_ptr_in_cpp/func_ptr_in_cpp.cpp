a0070ebf311cf67dd1cc9fa80858ca34cbe7d0f3
Jet (Ta-Wei Chien)$PUFF-X <67193495+jet-c-21@users.noreply.github.com>
2023-01-10 00:26:09 +0800

    fin ep59 lambda

--- ep58_func_ptr_in_cpp/func_ptr_in_cpp.cpp
+++ ep58_func_ptr_in_cpp/func_ptr_in_cpp.cpp
@@ -5,6 +5,8 @@
 #include "func_ptr_in_cpp.hpp"
 #include <iostream>
 #include <vector>
+#include <functional>
+#include <algorithm>
 
 void HelloWorld(int a) {
   std::cout << "Hello World! Value: " << a << std::endl;
@@ -28,15 +30,22 @@ void PrintValue(int value) {
   std::cout << "Value: " << value << std::endl;
 }
 
-void ForEach(const std::vector<int> &values, void(*func)(int)) {
+void ForEach(const std::vector<int> &values, const std::function<void(int)> &func) {
+//  void(*func)(int)
   for (int val : values)
     func(val);
 }
 
 void func_ptr_in_cpp_main_2() {
   std::vector<int> values = {1, 5, 4, 2, 3};
+  auto it = std::find_if(values.begin(), values.end(), [](int value) { return value > 3; });
+  std::cout << *it << std::endl;
+
   ForEach(values, PrintValue);
 
-  ForEach(values, [](int value) { std::cout << "Val = " << value << std::endl; });
+  int a = 5;
+
+  auto lambda = [=](int value) { std::cout << "Val = " << a << std::endl; };
+  ForEach(values, lambda);
 
 }
