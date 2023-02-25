415e85fc9c249084769357189e063753632cb9d5
Jet (Ta-Wei Chien)$PUFF-X <67193495+jet-c-21@users.noreply.github.com>
2023-02-25 23:36:06 +0800

    up

---
+++ ep64_nd_array_in_cpp/nd_array_in_cpp.cpp
@@ -0,0 +1,9 @@
+//
+// Created by puff on 2023-01-17.
+//
+
+#include "nd_array_in_cpp.hpp"
+
+void nd_array_in_cpp_main() {
+
+}
---
+++ ep64_nd_array_in_cpp/nd_array_in_cpp.hpp
@@ -0,0 +1,10 @@
+//
+// Created by puff on 2023-01-17.
+//
+
+#ifndef THECHERNOCPP_EP64_ND_ARRAY_IN_CPP_ND_ARRAY_IN_CPP_HPP_
+#define THECHERNOCPP_EP64_ND_ARRAY_IN_CPP_ND_ARRAY_IN_CPP_HPP_
+
+#endif //THECHERNOCPP_EP64_ND_ARRAY_IN_CPP_ND_ARRAY_IN_CPP_HPP_
+
+void nd_array_in_cpp_main();
